275cc1aa4ec726278e76e4ac365ea1a3880a88d3
AshishSidana <ashish.sidana@gmail.com>
2022-03-01 18:20:01 -0500

    test fix

--- tests/bootstrap_pages_test.py
+++ tests/bootstrap_pages_test.py
@@ -1,14 +1,14 @@
 """This test the bootstrappage"""
 
 
-def test_request_index(client):
+def test_request_home(client):
     """This makes the index page"""
     response = client.get("/home")
     assert response.status_code == 200
     assert b"Layout" in response.data
 
 
-def test_request_index(client):
+def test_request_about(client):
     """This makes the index page"""
     response = client.get("/about")
     assert response.status_code == 200
