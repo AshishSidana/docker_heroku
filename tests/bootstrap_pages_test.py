3775fae23c02bc29dc778fae0dd3af5532b4d90d
AshishSidana <98299599+AshishSidana@users.noreply.github.com>
2022-03-01 17:24:31 -0500

    Revert "test added"

--- tests/bootstrap_pages_test.py
+++
@@ -1,8 +0,0 @@
-"""This test the bootstrappage"""
-
-
-def test_request_index(client):
-    """This makes the index page"""
-    response = client.get("/bootstrap")
-    assert response.status_code == 200
-    assert b"Layout" in response.data
